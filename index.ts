27c983599a9f59f6da50278a01c149c53f9c0e14
Lakshay panwar <138835857+1amhellboy@users.noreply.github.com>
2026-06-24 23:04:28 +0530

    signup route added

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from 'express';
+const app = express();
+
+const users = [];
+
+app.post("/signup",(req,res)=>{
+	const { username, password } = req.body;
+	users.push({username, password});
+
+	req.json({message:"done"});
+});
+
+app.listen(3000);
