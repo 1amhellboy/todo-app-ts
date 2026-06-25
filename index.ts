3b0eee341d5f83be7c1e418506089e5780435e71
Lakshay panwar <138835857+1amhellboy@users.noreply.github.com>
2026-06-25 11:35:32 +0530

    duplicate signup fixed

--- index.ts
+++ index.ts
@@ -8,6 +8,11 @@ app.use(express.json());
 
 app.post("/signup",(req,res)=>{
 	const { username, password } = req.body;
+	const user = users.find(u => u.username === username);
+	if(user){
+		res.status(404).json({message:"user with this username already exists"});
+	}
+
 	users.push({username, password});
 
 	res.json({message:"done"});
